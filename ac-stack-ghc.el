279d994a659d78952647b418985125d0ce76c2c8
miyazaki <miyazaki_masayasu_f5@lab.ntt.co.jp>
2015-12-17 16:30:20 +0900

    add keyword

--- ac-stack-ghc.el
+++ ac-stack-ghc.el
@@ -2,7 +2,7 @@
 (defvar ac-stack-ghc-keywords
   '((candidates . (list "case" "class" "data" "default" "deriving" "else"
 		       "import" "infix" "infixl" "infixr" "instance" "let" "module" "newtype" "then" "type" "where"
-		       "Nothing" "undefined"))))
+		       "Nothing" "qualified" "return" "undefined"))))
 
 (defun load-dictionary (file)
   (let (dict)
@@ -19,16 +19,21 @@
 
 (defvar ac-dict-path  (expand-file-name "ac-stack-ghc-dict" (file-name-directory ac-stack-ghc-dictionary-directory)))
 
-(defvar haskell-mod-dict (load-dictionary ac-dict-path))
+(defvar haskell-mod-dict-cache (load-dictionary ac-dict-path))
 
 (defvar ac-source-haskell-import
-  '((candidates . haskell-mod-dict)
+  '((candidates . haskell-mod-dict-cache)
     (prefix . "^import \\(.*\\)")))
+
+(defvar ac-source-haskell-import-qulified
+  '((candidates . haskell-mod-dict-cache)
+    (prefix . "^import qualified \\(.*\\)")))
 
 (add-hook 'stack-ghci-mode-hook
 	  '(lambda ()
 	     (progn
+	       (add-to-list 'ac-sources 'ac-stack-ghc-keywords t)
 	       (add-to-list 'ac-sources 'ac-source-haskell-import t)
-	       (add-to-list 'ac-sources 'ac-stack-ghc-keywords t))))
+	       (add-to-list 'ac-sources 'ac-source-haskell-import-qulified t))))
 
 (provide 'ac-stack-ghc)
